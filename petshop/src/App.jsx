1ed0f137580198c87125322b17d4bc0da86d8a6b
João Nunes Rios <joao.rios@compasso.com.br>
2022-09-15 09:55:33 -0300

    Added header component

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './assets/css/base/base.css'
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 import Pagina404 from './paginas/Pagina404'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Cabecalho from './components/Cabecalho'
 
 function App() {
   return (
   <Router>
+    <Cabecalho />
     <Switch>
       <Route exact path='/'>
         <Home />
